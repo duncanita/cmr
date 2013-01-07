e5cb3336c7dbf61949231f1960d6b855a9f39b4e
Magomerlino74 <magomerlino74@tiscali.it>
2013-01-07 21:08:17 +0100

    first devise configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Cmr::Application.routes.draw do
-  devise_for :users
-
-  get "home/index"
-
-  resources :users
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -54,7 +49,14 @@ Cmr::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'home#index'
+  authenticated :user do 
+    root :to => 'home#index' 
+  end
+
+  root :to => 'home#index'
+
+  devise_for :users
+  resources :users
 
   # See how all your routes lay out with "rake routes"
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+puts  'SETTING UP DEFAULT USER LOGIN'
+user = User.create! :name => 'First user', :email => 'user@example.com', :password => 'password', :password_confirmation => 'password',
+		:surname => 'surname', :piva => '12345'
+puts 'New user created: ' << user.name
+user2 = User.create! :name => 'Second user', :email => 'user2@example.com', :password => 'password', :password_confirmation => 'password',
+		:surname => 'surname2', :piva => '12345'
+puts 'New user created: ' << user2.name
